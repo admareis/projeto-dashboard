426ebdfb96ac38f7bc8a5a9d8acf0e1715bb4773
admareis <reis802@gmail.com>
2026-06-02 22:07:17 -0300

    converter velocidade criada, verificar conversão

--- script.js
+++ script.js
@@ -3,26 +3,48 @@ function mostrarDesafio(id) {
   document.getElementById(id).style.display = 'block'; // chama somente a div que for clicada // 
 }
 
-function calcularTemperatura(){
+function calcularTemperatura(){    
     const celsius = document.getElementById('celsius').value.trim();
     const fahrenheit = document.getElementById('fahrenheit').value.trim();
-    const resultado = document.getElementById('calculoTotal');
+    const resultado = document.getElementById('calculoTotal3');
     const totalCelsius = (celsius*1.8)+32;
     const totalFahrenheit = (fahrenheit-32)/1.8
 
     if (celsius !==""){
-        resultado.innerText = celsius + "ºC" + " equivalem a " + totalCelsius.toFixed(2) +"ºF";
+        resultado.innerText = celsius + "ºC equivalem a " + totalCelsius.toFixed(2) +"ºF";
     }
     else if (fahrenheit !== ""){
-        resultado.innerText = fahrenheit + "ºF" + " equivalem a " + totalFahrenheit.toFixed(2) +"ºC"
+        resultado.innerText = fahrenheit + "ºF equivalem a " + totalFahrenheit.toFixed(2) +"ºC"
     } 
     else{ 
-        resultado.innerText = "Digite um valor em algum dos campos.";
-
+        resultado.innerText = "Digite um valor em algum dos campos."
     }  
 }
-function reset() {
+function reset3() {
     document.getElementById("celsius").value = "";
     document.getElementById("fahrenheit").value = "";
-    document.getElementById("calculoTotal").textContent = "";
+    document.getElementById("calculoTotal3").textContent = "";
 }
+
+function converterVelocidade(){
+    const quilometros = document.getElementById('km').value.trim()
+    const milhas = document.getElementById('mp').value.trim()
+    const resultado = document.getElementById('calculoTotal4');
+    const totalKm = quilometros/0.621371;
+    const totalMilhas = milhas*1.60934133;
+
+    if (quilometros !==""){
+        resultado.innerText = quilometros + "Km equivalem a " + totalKm.toFixed(2) +"Mp/h";
+    }
+    else if (milhas !==""){
+        resultado.innerText = milhas + "Mp/h equivalem a " + totalMilhas.toFixed(2) + "Km/h" 
+    }
+    else {
+        resultado.innerText = "Digite um valor em algum dos campos."
+    }
+}  
+function reset4() {
+    document.getElementById("km").value = "";
+    document.getElementById("mp").value = "";
+    document.getElementById("calculoTotal4").textContent = "";
+} 
